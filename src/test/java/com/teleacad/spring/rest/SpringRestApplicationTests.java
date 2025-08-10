7ef7c57a753762c8fe47e022e6824a252f8014c4
Claudiu Stroe <claudiucls@gmail.com>
2025-08-10 13:18:05 +0300

    first commit

---
+++ src/test/java/com/teleacad/spring/rest/SpringRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teleacad.spring.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
